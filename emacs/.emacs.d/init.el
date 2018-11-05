a1f3f38f57ee9d7457e8e415612e577714bd53f7
Kira Evans <contact@kne42.me>
2018-11-05 00:40:31 -0800

    initial commit

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,94 @@
+;;-----------------------------------------------------------------------------
+;; Bootstrapping
+;;-----------------------------------------------------------------------------
+
+;; disable GNU Elpa
+(setq package-archives nil)
+
+
+;; install `straight'
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+;; install `use-package' macro
+;; provides the keyword `:straight'
+(straight-use-package
+ '(use-package :host github :repo "jwiegley/use-package"))
+
+;; default to lazy loading
+;; can be overridden by `:demand' keyword
+(setq use-package-always-defer t)
+
+
+;; configuration topic files
+(defconst topics-dir
+  (concat user-emacs-directory (file-name-as-directory "topics"))
+  "Topic file loadpath.")
+
+;; add it to our loading search path
+(add-to-list 'load-path topics-dir)
+
+
+;;-----------------------------------------------------------------------------
+;; General Utilities
+;;-----------------------------------------------------------------------------
+
+;; determine launch info
+(defconst *is-a-mac*
+  (eq system-type 'darwin)
+  "Is this running on OS X?")
+
+(defconst *is-carbon-emacs*
+  (and *is-a-mac* (eq window-system 'mac))
+  "Is this the Carbon port of Emacs?")
+
+(defconst *is-cocoa-emacs*
+  (and *is-a-mac* (eq window-system 'ns))
+  "Is this the Cocoa version of Emacs?")
+
+(defconst *is-linux*
+  (eq system-type 'gnu/linux)
+  "Is this running on Linux?")
+
+(defconst *from-app*
+  (display-graphic-p)
+  "Is this running from an application?")
+
+(defconst *from-term*
+  (not *from-app*)
+  "Is this running from a terminal?")
+
+
+;; `diminish' provides diminishing of minor modes by removing their modeline
+;; display
+;; `use-package' integration: `:diminish'
+(use-package diminish
+  :straight (:host github :repo "myrjola/diminish.el")
+  :demand)
+
+
+;; `el-patch' provides on-the-fly patching of functions from other packages
+;; via sexp-based diffs
+;; `use-package' integration: `:init/el-patch', `:config/el-patch'
+(use-package el-patch
+  :straight (:host github :repo "raxod502/el-patch" :branch "develop")
+  :demand)
+
+
+;;-----------------------------------------------------------------------------
+;; Topic Loading
+;;-----------------------------------------------------------------------------
+
+;; `settings' provides global settings, advice, and keybindings
+(require 'init-settings)
---
+++ emacs/.emacs.d/topics/init-settings.el
@@ -0,0 +1,88 @@
+(use-package kne42-settings
+  :init
+  (defadvice keyboard-escape-quit (around my-keyboard-escape-quit activate)
+    "`<escape>' only needs to be hit once instead of three times."
+    (let (orig-one-window-p)
+      (fset 'orig-one-window-p (symbol-function 'one-window-p))
+      (fset 'one-window-p (lambda (&optional nomini all-frames) t))
+      (unwind-protect
+          ad-do-it
+        (fset 'one-window-p (symbol-function 'orig-one-window-p)))))
+
+  (defadvice kill-region (before slick-cut activate compile)
+    "When called interactively with no active region, kill a single line instead."
+    (interactive
+     (if mark-active
+         (list (region-beginning) (region-end))
+       (list (line-beginning-position) (line-beginning-position 2)))))
+
+  (defadvice delete-region (before slick-cut activate compile)
+    "When called interactively with no active region, delete a single line instead."
+    (interactive
+     (if mark-active
+         (list (region-beginning) (region-end))
+       (list (line-beginning-position) (line-beginning-position 2)))))
+
+  (defadvice kill-ring-save (before slick-copy activate compile)
+    "When called interactively with no active region, copy a single line instead."
+    (interactive
+     (if mark-active
+         (list (region-beginning) (region-end))
+       (message "Copied line")
+       (list (line-beginning-position) (line-beginning-position 2)))))
+
+  (defadvice comment-or-uncomment-region (before mark-whole-line activate compile)
+    "When called interactively with no active region, toggle a single line instead."
+    (interactive
+     (if mark-active
+         (setq beg (line-beginning-position) end (line-end-position)))))
+
+  (setq
+   load-prefer-newer t                      ; prefer newer .el over .elc
+   vc-follow-symlinks t                     ; follow symlinks to their targets
+   delete-selection-mode t                  ; modifying text replaces the region
+   kill-ring-max 5000                       ; truncate kill ring at 5000 entries
+   mark-ring-max 5000                       ; truncate mark ring at 5000 entries
+   visible-bell t                           ; turn off audio bell
+   global-hl-line-mode t                    ; highlight cursor line
+   blink-cursor-mode -1                     ; no blinking cursor
+   track-eol nil                            ; cursor doesn't track end-of-line
+   mouse-yank-at-point t                    ; paste at cursor position
+   sentence-end-double-space nil            ; sentences end with one space
+   truncate-partial-width-windows nil       ; don't truncate long lines
+   require-final-newline t                  ; add newline at the end of every file
+   global-linum-mode t                      ; show line numbers on buffers
+   column-number-mode t                     ; show column number in the mode-line
+   show-paren-mode t                        ; highlight parenthesis pairs
+   blink-matching-paren-distance nil        ; no blinking parenthesis
+   read-file-name-completion-ignore-case t  ; ignore case when completing file names
+   icomplete-mode t                         ; minibuffer completion
+   read-buffer-completion-ignore-case t)    ; ignore case when completing minibuffer
+
+  (setq-default
+   indicate-empty-lines t                   ; show empty lines
+   indent-tabs-mode nil                     ; use spaces instead of tabs
+   tab-width 4)                             ; tab length
+
+  (defalias 'yes-or-no-p 'y-or-n-p)         ; y/n instead of yes/no
+
+  (defvar delete-trailing-on-save t)        ; Delete trailing whitespaces on save.
+
+  (provide 'kne42-settings)
+
+  :hook
+  (before-save-hook . (lambda () (when delete-trailing-on-save
+                                   (delete-trailing-whitespace)))))
+
+:config
+;; app
+(if (display-graphic-p)
+    (setq tool-bar-mode -1                  ; no toolbar
+          menu-bar-mode -1                  ; no menubar
+          set-scroll-bar-mode 'right        ; set scrollbar right
+          scroll-bar-mode -1                ; disable scrollbar
+          scroll-preserve-screen-position t ; scroll without moving cursor
+          mouse-wheel-mode nil))            ; mouse-wheel disabled
+
+
+(provide 'init-settings)
